14a25583107533039acc954da9e8a51ba9a11338
root <root@DESKTOP-59TERCB.localdomain>
2024-11-06 20:35:01 +0200

    welcome message

--- finance_management.py
+++ finance_management.py
@@ -1,4 +1,7 @@
-print ("Wellcome to Financial Management App 💰💵")
-print ("This Financial Literacy Management App is an educational platform that equips people with essential financial skills through interactive content, quizzes, and exercises on budgeting, saving, and investing. It also connects users with new business and investment opportunities, promoting informed decision-making and fostering financial growth. The app empowers users to manage their finances responsibly and explore career or entrepreneurial paths.")
-print ("Fill the following simple steps and get knowledge on how to manage your finance properly ✅")
-print ("No more wasting money, we got you !!")
+def welcome_message():
+    print ("Wellcome to Financial Management App 💰💵\n")
+    print ("This Financial Literacy Management App is an educational platform that equips people with essential financial skills through interactive content, quizzes, and exercises on budgeting, saving, and investing. It also connects users with new business and investment opportunities, promoting informed decision-making and fostering financial growth. The app empowers users to manage their finances responsibly and explore career or entrepreneurial paths.\n")
+    print ("Fill the following simple steps and get knowledge on how to manage your finance properly ✅")
+    print ("No more wasting money, we got you!! 🤝 ")
+
+welcome_message()
