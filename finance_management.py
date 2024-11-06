d4ee4b4996fce5d97be6c63cf9ae3b5558429b40
root <root@DESKTOP-59TERCB.localdomain>
2024-11-06 19:38:52 +0200

    creating a python file

---
+++ finance_management.py
@@ -0,0 +1,4 @@
+print ("Wellcome to Financial Management App 💰💵")
+print ("This Financial Literacy Management App is an educational platform that equips people with essential financial skills through interactive content, quizzes, and exercises on budgeting, saving, and investing. It also connects users with new business and investment opportunities, promoting informed decision-making and fostering financial growth. The app empowers users to manage their finances responsibly and explore career or entrepreneurial paths.")
+print ("Fill the following simple steps and get knowledge on how to manage your finance properly ✅")
+print ("No more wasting money, we got you !!")
